94c58767996cfb56600cd715d308591ea6eafe7d
Stan238 <127895826+Stan238@users.noreply.github.com>
2024-06-25 15:35:11 -0400

    Initial Commit

---
+++ Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by Stan U on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
